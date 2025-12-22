a8e06e3683e7524f5cbdf54fb2f459a2daa949ae
DHRUV PATEL <dhruvpatel27106@gmail.com>
2025-12-22 11:49:09 +0530

    Add files via upload

---
+++ shopkeeper_ques.php
@@ -0,0 +1,8 @@
+<?php
+    // 11. One shopkeeper selling 5kg sweet with rs 430 per kg to 15 persons in 1 day write a php
+    //  code to calculate amount of sweet shopkeeper selling in one month.
+    $sweet = 5;
+    $rs = 430;
+    $persons = 15;
+    echo "Total amount shopkeeper selling in one month is : " . $sweet * $rs * $persons * 30;
+?>
