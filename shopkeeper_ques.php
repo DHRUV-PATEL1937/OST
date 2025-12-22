626142ce93007ca8a3939d74690c9b481d943d59
DHRUV PATEL <dhruvpatel27106@gmail.com>
2025-12-22 11:55:58 +0530

    changes done.

--- shopkeeper_ques.php
+++ shopkeeper_ques.php
@@ -4,5 +4,7 @@
     $sweet = 5;
     $rs = 430;
     $persons = 15;
-    echo "Total amount shopkeeper selling in one month is : " . $sweet * $rs * $persons * 30;
+    echo "Total amount shopkeeper selling in one month is : " . $sweet * $rs * $persons * 30 . " and " . $sweet*30 . " Kg sweet " . " to " . $persons*30 . " persons. <br><br>";
+    echo "Total amount shopkeeper selling in one week is : " . $sweet * $rs * $persons * 7 . " and " . $sweet*7 . " Kg sweet " . " to " . $persons*7 . " persons. <br><br>";
+    echo "Total amount shopkeeper selling in one day is : " . $sweet * $rs * $persons . " and " . $sweet . " Kg sweet " . " to " . $persons . " persons. <br>";
 ?>
