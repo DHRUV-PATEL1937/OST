5dd379c42917fe7df1edab295c2e5004db5d7790
DHRUV PATEL <dhruvpatel27106@gmail.com>
2026-01-19 09:24:34 +0530

    Delete use_of_echo_and_print.php

--- use_of_echo_and_print.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    echo "Hii" , " Hello";
-    // print "Hii" , "Hello";
-    $returnvar = print "<br>Hey";
-    echo $returnvar;
-    // $returnvar = echo "<br>Hey";
-    // echo $returnvar;
-?>
