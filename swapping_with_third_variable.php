495b4e78ccaf907a3060192c870a0c80791d004d
DHRUV PATEL <dhruvpatel27106@gmail.com>
2026-01-19 09:24:15 +0530

    Delete swapping_with_third_variable.php

--- swapping_with_third_variable.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $a = 10;
-    $b = 20;
-    echo "Before swap A = " . $a ." and B = ". $b;
-    $c = $a;
-    $a = $b;
-    $b = $c;
-    echo "<br>After swap A= ". $a ." and B = ". $b;
-?>
